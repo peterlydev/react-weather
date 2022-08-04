d2f6ae32fa81f3b3ea0af9ad44091b36bd329e22
Peter Ly <peter.ly1@hotmail.com>
2022-08-05 00:37:14 +0100

    added new features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Search from './components/search/Search';
+import Weather from './components/current-weather/Weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon ] = searchData.value.split(" ")
+    
+    const weatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all([weatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   }
+
+  console.log(currentWeather)
+  console.log(forecast)
   
   return (
     <div className="container">
     <Search onSearchChange={handleOnSearchChange} />
+    {currentWeather && <Weather data={currentWeather} />}
     </div>
   );
 }
--- src/components/current-weather/Weather.js
+++ src/components/current-weather/Weather.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Weather.css";
 
-const Weather = () => {
+const Weather = ({ data }) => {
   return (
-    <div>Weather</div>
-  )
-}
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-desc">{data.weather[0].description}</p>
+        </div>
+        <img src="icons/night.png" alt="weather" className="weather-icon" />
+      </div>
+      <div className="bottom">
+        <p className="temp">20°</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels Like</span>
+            <span className="parameter-value">25°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">25°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Weather
+export default Weather;
